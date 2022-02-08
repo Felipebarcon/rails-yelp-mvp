89891a66fbd8aabb36f9c84ed2e018aece276040
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-08 20:12:43 +0100

    validations ok

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,29 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
